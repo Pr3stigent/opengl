b74f05ae36c8387e6e26a065943f90cdd20ea058
Pr3stigent <83563230+Pr3stigent@users.noreply.github.com>
2022-01-03 18:59:07 +0000

    update premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+workspace "Workspace"
+    architecture "x64"
+
+    configurations {
+        "Debug",
+        "Release"
+    }
+
+    startproject "PracticeProject"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "PracticeProject"
+
+newaction {
+    trigger = "clean",
+    description = "Remove all binaries and intermediate binaries, and vs files.",
+    execute = function()
+        print("Removing binaries")
+        os.rmdir("./bin")
+        print("Removing intermediate binaries")
+        os.rmdir("./bin-int")
+        print("Removing project files")
+        os.rmdir("./.vs")
+        os.remove("**.sln")
+        os.remove("**.vcxproj")
+        os.remove("**.vcxproj.filters")
+        os.remove("**.vcxproj.user")
+        print("Done")
+    end
+}
